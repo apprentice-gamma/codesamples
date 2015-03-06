a41da89f01f93241e77fb4925706a13dee41ff37
Robb <robb.tvorik@detroitlabs.com>
2015-03-05 20:25:12 -0500

    fixed duplicate test

--- palindrome.js
+++ palindrome.js
@@ -14,7 +14,7 @@ function isPalindrome(word) {
 
 var test1 = isPalindrome("tacocat") === true;
 var test2 = isPalindrome("taco cat") === true;
-var test3 = isPalindrome("tacocat") === true;
+var test3 = isPalindrome("tacoOOoOoOOoOcat") === true;
 var test4 = isPalindrome("beep boop") === false;
 var test5 = isPalindrome("taCoc a T") === true;
 var test6 = isPalindrome("this is a PalnIDro236Mewty wrwqqwq...atry") === false;
