52fa04429927e7300fce5b4ee9f6bb22bddaa6b2
Robb <robb.tvorik@detroitlabs.com>
2015-03-05 20:20:06 -0500

    added some tests

--- palindrome.js
+++ palindrome.js
@@ -17,9 +17,12 @@ var test2 = isPalindrome("taco cat") === true;
 var test3 = isPalindrome("tacocat") === true;
 var test4 = isPalindrome("beep boop") === false;
 var test5 = isPalindrome("taCoc a T") === true;
+var test6 = isPalindrome("this is a PalnIDro236Mewty wrwqqwq...atry") === false;
+var test7 = isPalindrome("Red root put up to order") === true;
+
 
 function runTests() {
-	var tests = [test1, test2, test3, test4, test5];
+	var tests = [test1, test2, test3, test4, test5, test6, test7];
 	for(i=0; i < tests.length; i++) {
 		console.log("\n " + "Test: " + i + " is " + tests[i]);
 	}
