f3f65013e7b7caa6ff889a0ccb4d2a265c01f54f
Robb <robb.tvorik@detroitlabs.com>
2015-03-05 21:09:11 -0500

    small refactor

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,5 @@
 //**TODO**
 //refactor regex to handle ',' & '.' etc....
-
-function isPalindrome(word) {
-	//prepping the input, lowercase, remove spaces, and turn into an array with split()
-
-	var input = word.toLowerCase().replace(/ +/g, "").split("");
-
-	while (input.length-1 > 0) {
-		if (input.shift() !== input.pop()) return false; 
-	} 
-	return true; 
-}
-
 var test1 = isPalindrome("tacocat") === true;
 var test2 = isPalindrome("taco cat") === true;
 var test3 = isPalindrome("tacoOOoOoOOoOcat") === true;
@@ -20,6 +8,14 @@ var test5 = isPalindrome("taCoc a T") === true;
 var test6 = isPalindrome("this is a PalnIDro236Mewty wrwqqwq...atry") === false;
 var test7 = isPalindrome("Red root put up to order") === true;
 
+function isPalindrome(word) {
+	//prepping the input, lowercase, remove spaces, and turn into an array with split()
+	var input = word.toLowerCase().replace(/ +/g, "").split("");
+	while (input.length-1 > 0) {
+		if (input.shift() !== input.pop()) return false; 
+	} 
+	return true; 
+}
 
 function runTests() {
 	var tests = [test1, test2, test3, test4, test5, test6, test7];
